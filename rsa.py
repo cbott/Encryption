cd670e015ffeeedf2ec5c117f9f49ecfe2dfa9a8
Colin <cbott6@gmail.com>
2015-05-28 22:44:59 -0400

    readability

--- rsa.py
+++ rsa.py
@@ -12,7 +12,10 @@ primes = [18966162103181877137104002000766961367217593615563,
           97038736927236393600823373710502730009860820708979,
           35963516379667279099632691498218709389825006221563,
           39074083144091764365782913889746140369634051774269,
-          87829406556868104208432501228923251337658475933903]
+          87829406556868104208432501228923251337658475933903,
+          99701007192486571157506448414512864891510440378097,
+          22562323080364605847165435922793394781582610307019,
+          72166658850906333926882447749629659951480345596937]
           
 #egcd() and modinv() courtesy of stackoverflow
 def egcd(a, b):
@@ -44,8 +47,8 @@ def decrypt(num, priv_key, pub_key2):
     return pow(num, priv_key, pub_key2)
 
 ###generate keys###
-p = random.choice(primes)
-q = random.choice(primes)
+p = 21347#random.choice(primes)
+q = 24593#random.choice(primes)
 
 n = p*q #n is multiple of 2 large primes
 tn = (p-1)*(q-1)
@@ -53,18 +56,35 @@ tn = (p-1)*(q-1)
 e = 65537#coprime(tn)
 d = modinv( e , tn )
 
-print("Public Keys:",e,",",n)
-print("Private key d:",d)
 #private: d, p, q, tn
+#public: e, n
 
-message = input("\nMessage:")
+#public
+print("Alice wants to send a message to Bob")
+print("\nPublicly transmitted from Bob to Alice:")
+print("Public Keys:",e,",",n)
+
+#sender
+print("\nPrivate on Bob's machine:")
+
+message = input("Message:")
 
 number_msg = s2n(message)
-
-print("\nEncrypting:",number_msg)
+print("Encrypting:",number_msg)
 encrypted = encrypt(number_msg, e, n)
+
+#public again
+print("\nBob's public tranmission to Alice")
 print("Encrypted Message:",encrypted)
 
-pk = int(input("\nEnter private key (d):"))
+print("\nEve attempts to read message:")
+pk = int(input("Enter private key (hint, you can't, but you can try):"))
 decrypted = decrypt(encrypted, pk, n)
-print("\nDecrypted Message:",n2s(decrypted))
+print("Eve's Decrypted Message:",n2s(decrypted))
+
+#receiver
+print("\nPrivate on Alice's machine")
+print("Private key d:",d)
+pk = int(input("Enter private key (d):"))
+decrypted = decrypt(encrypted, pk, n)
+print("Alice's Decrypted Message:",n2s(decrypted))
